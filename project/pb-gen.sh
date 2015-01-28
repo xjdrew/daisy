e6697755dca076b7130b160b8e33715b549db1ef
xjdrew <xj.drew@gmail.com>
2015-01-28 21:01:01 +0800

    优化 pb-gen.sh

--- project/pb-gen.sh
+++ project/pb-gen.sh
@@ -4,13 +4,14 @@ set -xe
 PB_IN_DIR=$PWD/contrib/proto
 PB_OUT_DIR=$PWD/pb
 
-subdirs=`cd $PB_IN_DIR && find -L . -type d | sed 's/^\.\///' |grep -v '^\.$'`
+#subdirs=`cd $PB_IN_DIR && find -L . -type d | sed 's/^\.\///' |grep -v '^\.$'`
+subdirs=`find -L $PB_IN_DIR -type d -printf '%P\n'`
 for dir in $subdirs;do
     in_dir=$PB_IN_DIR/$dir
     out_dir=$PB_OUT_DIR/$dir
     mkdir -p $out_dir
 
-    proto_file=`cd $in_dir && find . -name '*.proto'`
+    proto_file=`find $in_dir -name '*.proto' -printf '%P\n'`
     if [ $proto_file ];then
         cd $in_dir && protoc --go_out=$out_dir $proto_file && cd - >/dev/null
     fi
