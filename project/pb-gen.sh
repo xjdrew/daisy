9726ba084eb7fe6dd42d2023782ad05e09edcc74
xjdrew <xj.drew@gmail.com>
2015-01-29 16:47:56 +0800

    convert *.proto to *.pb.go

--- project/pb-gen.sh
+++ project/pb-gen.sh
@@ -1,20 +1,51 @@
 #!/usr/bin/env bash
+
+# dependences: protoc-gen-go 
+# install: go get -u github.com/golang/protobuf/protoc-gen-go
+# The compiler plugin, protoc-gen-go, will be installed in $GOBIN,
+# defaulting to $GOPATH/bin.  It must be in your $PATH for the protocol
+# compiler, protoc, to find it.
+
 echo "------------- compile protobuffer file -------------"
 set -xe
 PB_IN_DIR=$PWD/contrib/proto
 PB_OUT_DIR=$PWD/pb
 
-#subdirs=`cd $PB_IN_DIR && find -L . -type d | sed 's/^\.\///' |grep -v '^\.$'`
-subdirs=`find -L $PB_IN_DIR -type d -printf '%P\n'`
+# convert *.proto to *.pb.go
+PROTO_FILES=
+cd $PB_IN_DIR
+subdirs=`find -L . -type d -printf '%P\n'`
 for dir in $subdirs;do
-    in_dir=$PB_IN_DIR/$dir
-    out_dir=$PB_OUT_DIR/$dir
-    mkdir -p $out_dir
-
-    proto_file=`find $in_dir -name '*.proto' -printf '%P\n'`
-    if [ $proto_file ];then
-        cd $in_dir && protoc --go_out=$out_dir $proto_file && cd - >/dev/null
+    proto_file=$(find $dir -name '*.proto')
+    if [ "$proto_file" ];then
+        protoc --go_out=$PB_OUT_DIR $proto_file
+        PROTO_FILES=${PROTO_FILES}" "${proto_file}
     fi
 done
+cd -
+
+# convert import XXX "path/to/file.pb" to import XXX "github.com/xjdrew/daisy/pb/path"
+PREFIX="github.com/xjdrew/daisy/pb/"
+function normalize_import() {
+    if [ -z "$1" ];then
+        return
+    fi
+
+    local file=$1
+    local tmpfile=${file}.old
+    for i in ${PROTO_FILES};do
+        local src=$(echo $i | sed 's/proto$/pb/')
+        local dst=${PREFIX}$(echo $src | sed 's/\/.*//')
+        mv $file $tmpfile
+        sed "/^import/s;$src;$dst;" $tmpfile > $file
+        rm $tmpfile
+    done
+}
+
+GO_FILES=$(find $PB_OUT_DIR -name "*.pb.go")
+for src in ${GO_FILES};do
+    normalize_import ${src}
+done
+
 set +xe
 echo "----------------------- end -----------------------"
